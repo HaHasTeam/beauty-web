3140b4c9c5be4ffdf960d852669d0a5a5783c58a
Phạm Công Minh <phamm5687@gmail.com>
2024-10-14 03:20:43 +0700

    feat: add error pages for 404, 403, and 500 status codes

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,7 +1,11 @@
-import Home from '@/views/Home'
 import { useRoutes } from 'react-router-dom'
 
 import About from '@/views/About'
+import Contact from '@/views/Contact'
+import Forbidden from '@/views/Forbidden'
+import Home from '@/views/Home'
+import NotFound from '@/views/NotFound'
+import ServerError from '@/views/ServerError'
 
 export default function RouterProvider() {
   return useRoutes([
@@ -12,6 +16,22 @@ export default function RouterProvider() {
     {
       path: '/about',
       element: <About />,
+    },
+    {
+      path: '/contact',
+      element: <Contact />,
+    },
+    {
+      path: '/not-found',
+      element: <NotFound />,
+    },
+    {
+      path: '/server-error',
+      element: <ServerError />,
+    },
+    {
+      path: '/forbidden',
+      element: <Forbidden />,
     },
   ])
 }
