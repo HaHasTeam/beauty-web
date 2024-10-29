11e0d2a00ed9c2b8210b52e5e7dc7e2e32c519aa
ngockim9047 <ngockim9047@gmail.com>
2024-10-30 00:21:55 +0700

    feat: add new contact page with responsive

--- src/views/About/index.tsx
+++ src/views/About/index.tsx
@@ -1,23 +1,5 @@
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-
-import { useUserStore } from '@/store/user'
-
-function About() {
-  const navigate = useNavigate()
-  const [pageTitle] = useState('laoer536-About Page')
-  const { num, changeNum } = useUserStore()
-
-  return (
-    <div>
-      <h1>{pageTitle}</h1>
-      <h2>userSore.num:{num}</h2>
-      <button onClick={changeNum}>click on the store provided by zustand to change the number</button>
-      <br />
-      <br />
-      <button onClick={() => navigate(-1)}>back</button>
-    </div>
-  )
+const About = () => {
+  return <div>About</div>
 }
 
 export default About
