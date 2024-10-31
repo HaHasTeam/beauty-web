0f4d0d844254f6b4bedf6ee19e77e8dc46b01a2a
ngockim9047 <ngockim9047@gmail.com>
2024-10-31 12:43:17 +0700

    style: apply linting and formatting updates to CSS

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,7 +1,6 @@
 import BeautyConsulting from '@/components/home/BeautyConsulting'
 import BeautyOffers from '@/components/home/BeautyOffers'
 
-const publicPath = import.meta.env.VITE_PUBLIC_PATH
 function Home() {
   return (
     <div className="w-[1200px] container mx-auto px-4 py-8 space-y-12">
