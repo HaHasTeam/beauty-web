ae26bc87cb89f1885a12339da7b387c44011eb33
Phạm Công Minh <phamm5687@gmail.com>
2024-11-05 09:34:12 +0700

    feat: 🎸 finish setup i18n

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,13 +1,13 @@
 import i18n from 'i18next'
 import detector from 'i18next-browser-languagedetector'
-import Backend from 'i18next-http-backend'
+import HttpApi from 'i18next-http-backend'
 import intervalPlural from 'i18next-intervalplural-postprocessor'
 import { initReactI18next } from 'react-i18next'
 
 export const supportedLngs = ['en', 'vi']
 
 i18n
-  .use(Backend)
+  .use(HttpApi)
   .use(detector)
   .use(initReactI18next)
   .use(intervalPlural)
