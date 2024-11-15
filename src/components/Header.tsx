ff381dd9dcb0c6d4e94bbebaab72bad99aa8acc8
ngockim9047 <ngockim9047@gmail.com>
2024-11-15 15:43:03 +0700

    feat: add-language-switch-to-header update-language-switcher and update-header-responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,14 @@
-import { ChevronDown, HelpCircle, Search, ShoppingBag } from 'lucide-react'
+import { Bell, HelpCircle, Menu, Search, ShoppingBag, X } from 'lucide-react'
+import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Link, useNavigate } from 'react-router-dom'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import configs from '@/config'
+import { ProjectInformationEnum } from '@/types/enum'
 
+import LanguageSwitcher from './LanguageSwitcher'
 import WebNotification from './notification/WebNotification'
 
 export default function Header() {
@@ -21,9 +24,9 @@ export default function Header() {
   }
   const navigate = useNavigate()
   const { t } = useTranslation()
-
+  const [menuOpen, setMenuOpen] = useState(false)
   return (
-    <header className="w-full bg-white">
+    <header className="w-full bg-white relative">
       <div className="">
         <div className="px-4 flex items-center justify-between py-2 text-sm">
           <div className="flex space-x-4">
@@ -44,17 +47,14 @@ export default function Header() {
               <HelpCircle className="h-4 w-4" />
               <span className="text-sm">{t('helpCenter.name')}</span>
             </Button>
-            <Button variant="ghost" size="sm" className="h-8 px-2">
-              <span className="text-sm">{t('language.english')}</span>
-              <ChevronDown className="ml-1 h-4 w-4" />
-            </Button>
+            <LanguageSwitcher />
           </div>
         </div>
-        <div className="px-4 flex items-center justify-between py-4 border-t border-secondary">
-          <Link to={configs.routes.home} className="text-3xl font-bold text-primary">
-            Allure
+        <div className="px-2 md:px-4 flex items-center justify-between py-4 border-t border-secondary">
+          <Link to={configs.routes.home} className="text-3xl font-bold text-primary hidden md:block">
+            {ProjectInformationEnum.name}
           </Link>
-          <div className="flex-1 px-12">
+          <div className="flex-1 px-2 md:px-12">
             <div className="relative">
               <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-gray-400" />
               <Input type="search" placeholder="Search..." className="w-full pl-10 pr-4" defaultValue="mask" />
@@ -66,7 +66,8 @@ export default function Header() {
               <span>lipstick</span>
             </div>
           </div>
-          <div className="flex items-center space-x-3">
+          {/* desktop menu */}
+          <div className="md:flex items-center space-x-3 hidden">
             <WebNotification
               notifications={notifications}
               notificationCount={notifications.length}
@@ -87,8 +88,50 @@ export default function Header() {
               <span className="sr-only">{t('header.shoppingCart')}</span>
             </Button>
           </div>
+          {/* Mobile Menu Toggle */}
+          <div className="flex md:hidden">
+            <Button variant="ghost" size="icon" onClick={() => setMenuOpen(!menuOpen)} className={`transition-colors `}>
+              {menuOpen ? <X className="h-6 w-6" /> : <Menu className="h-6 w-6" />}
+            </Button>
+          </div>
         </div>
       </div>
+      {/* Mobile Dropdown */}
+      {menuOpen && (
+        <div className="md:hidden absolute z-20 top-full left-0 w-full bg-white shadow-md border-t">
+          <div className="p-4">
+            <div className="flex gap-2">
+              <Button variant="ghost" onClick={() => navigate(configs.routes.notification)}>
+                <Bell className="text-gray-700" />
+                <span> {t('header.notification')}</span>
+              </Button>
+            </div>
+            <div className="flex gap-2">
+              <Button
+                variant="ghost"
+                className="flex justify-start"
+                onClick={() => {
+                  setMenuOpen(false)
+                  navigate(configs.routes.cart)
+                }}
+              >
+                <ShoppingBag className="h-5 w-5" />
+                <span>{t('header.shoppingCart')}</span>
+              </Button>
+            </div>
+            <Button
+              variant="default"
+              className="w-full text-primary-foreground"
+              onClick={() => {
+                setMenuOpen(false)
+                navigate(configs.routes.signIn)
+              }}
+            >
+              {t('header.loginOrRegister')}
+            </Button>
+          </div>
+        </div>
+      )}
     </header>
   )
 }
--- src/components/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher.tsx
@@ -1,23 +1,42 @@
+import { ChevronDown } from 'lucide-react'
+import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
+
+import { Button } from './ui/button'
 
 const LanguageSwitcher = () => {
   const { i18n } = useTranslation()
   const supportedLngs = i18n.options.supportedLngs || []
-
+  const [isOpenLanguage, setOpenLanguage] = useState(false)
+  const toggleDropdown = () => setOpenLanguage(!isOpenLanguage)
+  const handleChangeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng)
+    setOpenLanguage(false)
+  }
   return (
-    <div className="flex space-x-2">
-      {supportedLngs.map((lng) => (
-        <button
-          key={lng}
-          className={`px-4 py-2 rounded ${
-            i18n.resolvedLanguage === lng ? 'font-bold bg-blue-500 text-white' : 'font-normal bg-gray-200 text-gray-800'
-          }`}
-          type="button"
-          onClick={() => i18n.changeLanguage(lng)}
-        >
-          {lng.toUpperCase()}
-        </button>
-      ))}
+    <div className="flex space-x-2 relative">
+      <Button variant="ghost" size="sm" className="h-8 px-2" onClick={toggleDropdown}>
+        <span className="text-sm">{i18n.resolvedLanguage?.toUpperCase()}</span>
+        <ChevronDown className="ml-1 h-4 w-4" />
+      </Button>
+      {isOpenLanguage && (
+        <div className="absolute flex flex-col top-full right-0 mt-2 bg-white border border-gray-200 shadow-lg rounded-md z-10">
+          {supportedLngs.slice(0, -1).map((lng) => (
+            <Button
+              key={lng}
+              className={`rounded-none ${
+                i18n.resolvedLanguage === lng
+                  ? 'font-bold bg-primary text-primary-foreground'
+                  : 'font-normal bg-accent text-accent-foreground'
+              }`}
+              type="button"
+              onClick={() => handleChangeLanguage(lng)}
+            >
+              {lng.toUpperCase()}
+            </Button>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
