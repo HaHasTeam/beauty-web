8eaa087b81049c36267504108a652af270a482de
ngockim9047 <ngockim9047@gmail.com>
2024-12-24 23:05:01 +0700

    feat: add scroll to top component

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,12 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 import App from './App.tsx'
+import ScrollToTop from './components/scroll-to-top/index.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter basename={import.meta.env.VITE_PUBLIC_PATH as string}>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </StrictMode>,
