93ad19b65415aaf28c1b3c1d99e0a26afa11a864
ngockim9047 <ngockim9047@gmail.com>
2025-02-12 00:33:44 +0700

    style: update order status style

--- src/components/order-status/index.tsx
+++ src/components/order-status/index.tsx
@@ -12,6 +12,7 @@ export default function OrderStatus({ tag, text, size = 'medium' }: OrderStatusP
   const { t } = useTranslation()
 
   let tagColorClass = ''
+  let bgColorClass = ''
   let tagText = ''
   const sizeClasses = {
     small: 'px-1 text-xs',
@@ -23,57 +24,70 @@ export default function OrderStatus({ tag, text, size = 'medium' }: OrderStatusP
   switch (tag) {
     case ShippingStatusEnum.JOIN_GROUP_BUYING:
       tagColorClass = 'text-teal-400'
+      bgColorClass = 'bg-teal-100'
       tagText = t('order.joinGroupBuying')
       break
     case ShippingStatusEnum.TO_PAY:
-      tagColorClass = 'text-yellow-400'
+      tagColorClass = 'text-yellow-500'
+      bgColorClass = 'bg-yellow-100'
       tagText = t('order.pending')
       break
     case ShippingStatusEnum.WAIT_FOR_CONFIRMATION:
       tagColorClass = 'text-amber-500'
+      bgColorClass = 'bg-amber-100'
       tagText = t('order.waitConfirm')
       break
     case ShippingStatusEnum.TO_SHIP:
       tagColorClass = 'text-orange-600'
+      bgColorClass = 'bg-orange-100'
       tagText = t('order.shipping')
       break
     case ShippingStatusEnum.PREPARING_ORDER:
       tagColorClass = 'text-purple-600'
+      bgColorClass = 'bg-purple-100'
       tagText = t('order.preparingOrder')
       break
     case ShippingStatusEnum.TO_RECEIVED:
       tagColorClass = 'text-cyan-600'
+      bgColorClass = 'bg-cyan-100'
       tagText = t('order.delivering')
       break
     case ShippingStatusEnum.DELIVERED:
       tagColorClass = 'text-blue-600'
+      bgColorClass = 'bg-blue-100'
       tagText = t('order.delivered')
       break
     case ShippingStatusEnum.COMPLETED:
       tagColorClass = 'text-green-600'
+      bgColorClass = 'bg-green-100'
       tagText = t('order.completed')
       break
     case ShippingStatusEnum.CANCELLED:
       tagColorClass = 'text-red-600'
+      bgColorClass = 'bg-red-100'
       tagText = t('order.cancelled')
       break
     case ShippingStatusEnum.RETURNING:
       tagColorClass = 'text-indigo-600'
+      bgColorClass = 'bg-indigo-100'
       tagText = t('order.returning')
       break
     case ShippingStatusEnum.REFUNDED:
       tagColorClass = 'text-gray-600'
+      bgColorClass = 'bg-gray-100'
       tagText = t('order.refunded')
       break
 
     default:
-      tagColorClass = 'bg-gray-200 text-gray-800' // Default color
+      tagColorClass = 'bg-gray-100 text-gray-800' // Default color
       tagText = tag // Default to the tag string if no match is found
       break
   }
 
   return (
-    <span className={`${sizeClasses[size]} uppercase cursor-default font-bold ${tagColorClass}`}>
+    <span
+      className={`${sizeClasses[size]} p-2 ${bgColorClass} rounded-full uppercase cursor-default font-bold ${tagColorClass}`}
+    >
       {text ? text : tagText}
     </span>
   )
