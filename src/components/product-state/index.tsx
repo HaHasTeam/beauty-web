de4e02d920f8be9674d54962421c3b98c3790aef
ngockim9047 <ngockim9047@gmail.com>
2025-02-20 16:48:50 +0700

    feat: add product state and update order schema, update language

---
+++ src/components/product-state/index.tsx
@@ -0,0 +1,45 @@
+import { useTranslation } from 'react-i18next'
+
+import { ProductEnum } from '@/types/enum'
+
+interface ProductStateProps {
+  state: string
+  text?: string
+  size?: 'small' | 'medium' | 'large'
+}
+
+export default function ProductState({ state, text, size = 'medium' }: ProductStateProps) {
+  const { t } = useTranslation()
+
+  let tagColorClass = ''
+  const sizeClasses = {
+    small: 'px-1 text-xs',
+    medium: 'px-3 py-1 sm:text-sm text-xs',
+    large: 'p-3 lg:text-base md:text-sm sm:text-xs',
+  }
+
+  // Define color based on tag
+  switch (state) {
+    case ProductEnum.OFFICIAL: // for default address
+      tagColorClass = 'border-green-300 border bg-green-100 text-green-600'
+      break
+    case ProductEnum.INACTIVE:
+      tagColorClass = 'border border-gray-300 bg-gray-100 text-gray-600'
+      break
+    case ProductEnum.OUT_OF_STOCK:
+      tagColorClass = 'border border-red-300 bg-red-100 text-red-600'
+      break
+    case ProductEnum.PRE_ORDER:
+      tagColorClass = 'text-yellow-500 bg-yellow-50 border border-yellow-500'
+      break
+    default:
+      tagColorClass = 'bg-gray-200 text-gray-800' // Default color
+      break
+  }
+
+  return (
+    <span className={`${sizeClasses[size]} cursor-default rounded-full font-medium ${tagColorClass}`}>
+      {text ? text : t(`status.${state}`)}
+    </span>
+  )
+}
