f10562c03d203a56539d5a3dccd5028b28a47be9
ngockim9047 <ngockim9047@gmail.com>
2025-04-17 22:08:32 +0700

    fix: fix blog

--- src/network/apis/blog/index.ts
+++ src/network/apis/blog/index.ts
@@ -1,17 +1,23 @@
 import { IBlogDetails, IServerCreateBlog } from '@/types/blog'
 import { TServerResponse } from '@/types/request'
 import { toMutationFetcher, toQueryFetcher } from '@/utils/query'
-import { privateRequest } from '@/utils/request'
+import { privateRequest, publicRequest } from '@/utils/request'
 
 import { TGetFilteredReportRequestParams } from '../report/type'
 import { UpdateBlogParams } from './type'
 
 export const getAllBlogApi = toQueryFetcher<void, TServerResponse<IBlogDetails[]>>('getAllBlogApi', async () => {
-  return privateRequest('/blogs')
+  return publicRequest('/blogs')
 })
 export const getBlogApi = toQueryFetcher<string, TServerResponse<IBlogDetails>>('getBlogApi', async (productId) => {
   return privateRequest(`/blogs/get-by-id/${productId}`)
 })
+export const getBlogByTagApi = toQueryFetcher<string, TServerResponse<IBlogDetails>>(
+  'getBlogByTagApi',
+  async (productId) => {
+    return publicRequest(`/blogs/tag/${productId}`)
+  },
+)
 
 export const createBlogApi = toMutationFetcher<IServerCreateBlog, TServerResponse<IServerCreateBlog>>(
   'createBlogApi',
@@ -25,7 +31,7 @@ export const createBlogApi = toMutationFetcher<IServerCreateBlog, TServerRespons
 export const getFilteredBlogs = toQueryFetcher<TGetFilteredReportRequestParams, TServerResponse<IBlogDetails[]>>(
   'getFilteredBlogs',
   async (query) => {
-    return privateRequest('/blogs/filter-blogs', {
+    return publicRequest('/blogs/filter-blogs', {
       method: 'POST',
       data: query,
     })
